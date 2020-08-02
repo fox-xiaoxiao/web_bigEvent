abe4d4554c19a69c4f4b2b93141c8521d5bd18cc
fox <platycodon-zhao@outlook.com>
2020-08-02 10:00:00 +0800

    '修改并提交了user'

--- assets/js/user/user_avatar.js
+++ assets/js/user/user_avatar.js
@@ -31,4 +31,28 @@ $(function () {
             .attr('src', newImgURL)  // 重新设置图片路径
             .cropper(options)        // 重新初始化裁剪区域
     })
+    //头像上传
+    $('#btnUpload').on('click', function () {
+        var dataURL = $image
+            .cropper('getCroppedCanvas', { // 创建一个 Canvas 画布
+                width: 100,
+                height: 100
+            })
+            .toDataURL('image/png') // 将 Canvas 画布上的内容，转化为 base64 格式的字符串
+        $.ajax({
+            method: 'post',
+            url: '/my/update/avatar',
+            data: {
+                avatar: dataURL
+            },
+            success: function (res) {
+                if (res.status != 0) {
+                    return layui.layer.msg(res.message)
+                }
+                layui.layer.msg('头像上传成功')
+                //  刷新父框架的个人资料
+                window.parent.getUserInfo()
+            }
+        })
+    })
 })
